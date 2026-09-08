4278f3ab7e9fdeedea1894993e7998bc465cb919
venkat <Venkat.raman@gmail.com>
2026-09-08 15:44:37 +0200

    ADDED jwt filter

---
+++ src/main/java/com/venkat/bookmyshowapplication/auth/Security/JwtAuthenticationFilter.java
@@ -0,0 +1,94 @@
+package com.venkat.bookmyshowapplication.auth.Security;
+
+import com.venkat.bookmyshowapplication.Common.Exceptions.InvalidCredentialsException;
+import com.venkat.bookmyshowapplication.Common.Exceptions.InvalidRefreshTokenException;
+import com.venkat.bookmyshowapplication.User.Model.User;
+import com.venkat.bookmyshowapplication.User.Repository.UserRepository;
+import com.venkat.bookmyshowapplication.auth.model.RefreshToken;
+import com.venkat.bookmyshowapplication.auth.repository.RefreshTokenRepository;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Optional;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static String Prefix_value ="Bearer ";
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public JwtAuthenticationFilter(UserRepository userRepository, JwtService jwtService, RefreshTokenRepository refreshTokenRepository) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorisation");
+
+        if (header==null || !header.startsWith(Prefix_value)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String accessToken = getaccessToken(header);
+
+      try{
+
+          long userid = jwtService.extractUserId(accessToken);
+
+          Optional<User> user = userRepository.findById(userid);
+
+          if (user.isEmpty()){
+              throw new InvalidCredentialsException("No user id found");
+          }
+
+          Optional<RefreshToken> refreshToken = refreshTokenRepository.findById(userid);
+          if (!refreshToken.get().getExpiresAt().isBefore(Instant.now())){
+              throw  new InvalidRefreshTokenException("Invalid Token");
+          }
+          UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                  user.get().getEmail(),
+                  null,
+                  Collections.emptyList()
+
+          );
+          authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+          filterChain.doFilter(request,response);
+
+      }
+
+       catch (JwtException | IllegalArgumentException exception){
+           response.sendError(
+                   HttpServletResponse.SC_UNAUTHORIZED,
+                   "Invalid or expired access token"
+           );
+
+       }
+
+
+
+    }
+
+    private String getaccessToken(String Header){
+
+        return  Header.substring(Prefix_value.length()).trim();
+    }
+}
